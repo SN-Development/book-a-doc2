8264ddc5fbe515959d76c209e2213bd439463944
Shashindu <shashinduchamika99@gmail.com>
2023-07-04 18:52:56 +0530

    sn2

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <NavBar></NavBar>
       <WelcomeSlide></WelcomeSlide>
+      <WelcomeSlide></WelcomeSlide>
       <h1>Hello world</h1>
     </div>
   );
